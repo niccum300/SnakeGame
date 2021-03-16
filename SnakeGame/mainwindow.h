019bf2461a978f708498c7f4016055ccec515aa0
Weightyfugur <34199667+Weightyfugur@users.noreply.github.com>
2021-03-16 11:34:13 -0400

    New Project

--- SnakeGame/mainwindow.h
+++ SnakeGame/mainwindow.h
@@ -2,21 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QtOpenGL>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
